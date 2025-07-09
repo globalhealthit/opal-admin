dc745e6837fdff5be6a7afa57e8be88fda3c68fd
root <root@ip-172-31-25-226.us-west-2.compute.internal>
2025-07-09 11:05:54 +0000

    Added updates for domain and ssl

--- config/settings/development.py
+++ config/settings/development.py
@@ -22,7 +22,7 @@ SECRET_KEY = env.str(
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = [
-    '44.227.113.66', '.localhost', '127.0.0.1', '[::1]', 'host.docker.internal'
+    'opaladmin.shoprideon.com', '.localhost', '127.0.0.1', '[::1]', 'host.docker.internal'
 ]
 
 # CACHES
